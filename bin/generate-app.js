5598f873b1a013bed717b1133f0867136e7d9611
Abderraouf Belalia <92583956+abderraouf-belalia@users.noreply.github.com>
2023-06-05 06:22:35 -0700

    Update generate-app.js

--- bin/generate-app.js
+++ bin/generate-app.js
@@ -14,7 +14,7 @@ if (process.argv.length < 3) {
 const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
-const git_repo = "https://github.com/abderraouf-belalia/create-discord-app";
+const git_repo = "https://github.com/Destruct-Portfolio/create-discordx-app";
 
 try {
   fs.mkdirSync(projectPath);
